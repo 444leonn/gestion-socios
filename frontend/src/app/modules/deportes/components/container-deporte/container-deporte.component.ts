2cd79da8104dab818af06658e1dd34bee8a40d80
444leonn <acostaleon25@gmail.com>
2025-08-19 15:45:47 -0300

    feat: agregado component container de un deporte

---
+++ frontend/src/app/modules/deportes/components/container-deporte/container-deporte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "container-deporte",
+    standalone: true,
+    templateUrl: "./container-deporte.component.html",
+    styleUrls: ["./container-deporte.component.css"]
+})
+export class ContainerDeporte { 
+    
+};
