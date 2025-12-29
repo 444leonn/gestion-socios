b482ff61007bb25d110fe4b2028e0dbd00087546
444leonn <acostaleon25@gmail.com>
2025-12-29 14:47:55 -0300

    modfi: home card dynamic values

--- frontend/src/app/components/home-card/home-card.component.ts
+++ frontend/src/app/components/home-card/home-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+interface homeCard {
+    name: string,
+    logo: string,
+    spec: string,
+    color: string,
+}
 
 @Component({
   selector: 'app-home-card',
   imports: [],
   templateUrl: './home-card.component.html',
   styleUrl: './home-card.component.css',
+  host: {
+    '[style.--card-color]': 'cardInfo().color'
+  }
 })
 export class HomeCardComponent {
-
+    cardInfo = input.required<homeCard>();
 }
