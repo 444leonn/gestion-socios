f2b9d5514215869ef149d6ac6bdea6489f5f71e6
444leonn <acostaleon25@gmail.com>
2025-12-30 01:34:07 -0300

    feat: sportss-page frontend

---
+++ frontend/src/app/pages/sports/sports.page.ts
@@ -0,0 +1,28 @@
+import { Component, resource, signal } from '@angular/core';
+import { AddButtonComponent } from '../../components/add-button/add-button.component';
+import { AddSportModalComponent } from '../../components/add-sport-modal/add-sport-modal.component';
+import { SportCardComponent } from "../../components/sport-card/sport-card.component";
+import { Deporte } from '../../models/deporte';
+import { environment } from '../../../environments/environments';
+
+@Component({
+  selector: 'app-sports',
+  imports: [AddButtonComponent, AddSportModalComponent, SportCardComponent],
+  templateUrl: './sports.page.html',
+  styleUrl: './sports.page.css',
+})
+export class SportsPage {
+  mostrar = signal(false);
+  private apiUrl = `${environment.apiUrl}/deportes`;
+  
+  Deportes = resource({
+    loader: async () => {
+      const response = await fetch(this.apiUrl);
+
+      if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+      }
+      return response.json() as Promise<Deporte[]>
+    }
+  });
+}
