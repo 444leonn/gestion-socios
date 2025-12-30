1c2a329485d1adbc59677f32372f01795f3478cc
444leonn <acostaleon25@gmail.com>
2025-12-30 19:46:36 -0300

    feat: icon library modal

---
+++ frontend/src/app/components/icons-library/icons-library.component.ts
@@ -0,0 +1,19 @@
+import { Component, input, output } from '@angular/core';
+import { IconsCardComponent } from "../icons-card/icons-card.component";
+import sportIcons from '../../../../public/sport-icons.json';
+
+@Component({
+  selector: 'app-icons-library',
+  imports: [IconsCardComponent],
+  templateUrl: './icons-library.component.html',
+  styleUrl: './icons-library.component.css',
+})
+export class IconsLibraryComponent {
+  close = output<void>();
+
+  iconNames: string[] = sportIcons.icons;
+
+  handleClick(): void {
+    this.close.emit();
+  }
+}
