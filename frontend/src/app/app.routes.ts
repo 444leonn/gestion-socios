2b34ca1ebdee9778b8cc6354e142ee2e2698a751
444leonn <acostaleon25@gmail.com>
2026-01-10 00:26:03 -0300

    feat: add initial socios page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { HomePage } from './pages/home/home.page';
 import { SportsPage } from './pages/sports/sports.page';
 import { NotFoundPage } from './pages/not-found/not-found.page';
+import { SociosPage } from './pages/socios/socios.page';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePage,
+    },
+    {
+        path: 'socios',
+        component: SociosPage,
     },
     {
         path: 'deportes',
---
+++ frontend/src/app/pages/socios/socios.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-socios',
+  imports: [],
+  templateUrl: './socios.page.html',
+  styleUrl: './socios.page.css',
+})
+export class SociosPage {
+
+}
